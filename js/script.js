3f5b53e6be657175eb8d938d7259ae6d077705fb
Gennady Ulasen <8581807>
2021-02-02 16:29:52 +0300

    commit slider

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-var swiper = new Swiper('.clients-wrapper',  {
+let mySwiper =new Swiper('.client',  {
   slidesPerView: 6,
-  spaceBetween: 15,
-  slidesPerGroup: 6,
+  spaceBetween: 5,
+  slidesPerGroup: 2,
   loop: true,
   loopFillGroupWithBlank: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+});
+
+
+let swiper = new Swiper('.swiper-portfolio',  {
+  slidesPerView: 3,
+  pagination: {
+    el: '.porfolio-pagination',
+    clickable: true,
   },
 });
